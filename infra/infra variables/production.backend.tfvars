55034dcbdf653b99032eccd1880a747823399bbb
RustyTake-Off <109174317+RustyTake-Off@users.noreply.github.com>
2023-03-27 01:33:39 +0200

    Move infra files

---
+++ infra/infra variables/production.backend.tfvars
@@ -0,0 +1,6 @@
+##################################################
+# Azurerm backend configuration
+resource_group_name  = "rgpr08pro"
+storage_account_name = "strapr08pro"
+container_name       = "contpr08pro"
+key                  = "prod.08pro.tfstate"
---
+++ infra/module.tf
@@ -0,0 +1,11 @@
+##################################################
+# Module caf - configuration
+module "caf" {
+  source  = "aztfmod/caf/azurerm"
+  version = "~> 5.0"
+
+  global_settings = var.global_settings
+  resource_groups = var.resource_groups
+
+
+}
